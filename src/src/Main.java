ff613d3d847454997bd6d9e74b95fef23f26bb35
John <johnjohn200513@gmail.com>
2024-11-16 15:23:52 -0300

    tst

--- src/src/Main.java
+++ src/src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.printf("os %d viados%n", 6);
         System.out.println("Lula Fraco");
+        System.out.printf("os %d viados%n", 6);
     }
 
 }
